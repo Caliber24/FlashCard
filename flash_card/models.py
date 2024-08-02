fe1c2950c9f6d2ff1ef224f84f0e2db90f41fd4e
Caliber24 <amirhosseinpishroa2001@gmail.com>
2024-08-02 18:49:30 +0330

    create flash-card model

--- flash_card/models.py
+++ flash_card/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class FlashCard(models.Model):
+  question = models.TextField(verbose_name='سوال')
+  answer = models.TextField(verbose_name='پاسخ')
+  create_at = models.DateTimeField(auto_now_add=True)
+  update_at = models.DateTimeField(auto_now_add=True)
+  class Meta:
+      verbose_name = ("فلش کارت")
+      verbose_name_plural = ("فلش کارت ها")
+
+  def __str__(self):
+      return f"{self.question[0:10]}..."
+
+  def get_absolute_url(self):
+      return reverse("_detail", kwargs={"pk": self.pk})
+
+  
