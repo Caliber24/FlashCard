406da9c017cc2c6d94adc91f97744c775da31281
Caliber24 <amirhosseinpishroa2001@gmail.com>
2024-08-02 22:24:51 +0330

    create register user api

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+# from rest_framework.generics import CreateAPIView
+from django.contrib.auth.models import User
+from .serializers import CreateUserProfileSerializer
+# class CreateUser(CreateAPIView):
+#   queryset=User.objects.all()
+#   serializer_class = CreateUserProfileSerializer
 
-# Create your views here.
+class CreateUser(APIView):
+  def post(self, request):
+    req_data = request.data
+    
+    serializer = CreateUserProfileSerializer(data=req_data)
+    serializer.is_valid(raise_exception=True)
+    data = serializer.validated_data
+    user_data = User(
+      username = data['username'],
+      email=data['email']
+    )
+    user_data.set_password(data['password'])
+    user_data.save()
+    
+    return Response(serializer.data)
