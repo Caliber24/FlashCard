c1801b35b6dca4dd21661c0e1106f4e231037a46
Caliber24 <amirhosseinpishroa2001@gmail.com>
2024-09-06 10:46:29 +0330

    finish

--- flash_card/views.py
+++ flash_card/views.py
@@ -1,5 +1,5 @@
 from rest_framework.views import APIView
-from flash_card.serializers import CreateFlashCard, UpdateFlashCard , ListFlashCard
+from flash_card.serializers import CreateFlashCard, UpdateFlashCard, ListFlashCard
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404, get_list_or_404
 from flash_card.models import FlashCard
@@ -16,29 +16,31 @@ class CreateFlashCardView(APIView):
 
 
 class UpdateFlashCardView(APIView):
-  permission_classes=(IsAuthenticated,)
-  def put(self, request, id):
-    flash_card = get_object_or_404(FlashCard, id=id)
-    serializer = UpdateFlashCard(data=request.data, instance=flash_card)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-    return Response(serializer.data)
+    permission_classes = (IsAuthenticated,)
+
+    def put(self, request, id):
+        flash_card = get_object_or_404(FlashCard, id=id)
+        serializer = UpdateFlashCard(data=request.data, instance=flash_card)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
 
 class DeleteFlashCardView(APIView):
-  permission_classes=(IsAuthenticated,)
-  def delete(self, request, id):
-      flash_card = get_object_or_404(FlashCard, id=id)
-      flash_card.delete()
-      return Response(status=status.HTTP_202_ACCEPTED)
-      
-      
+    permission_classes = (IsAuthenticated,)
+
+    def delete(self, request, id):
+        flash_card = get_object_or_404(FlashCard, id=id)
+        flash_card.delete()
+        return Response(status=status.HTTP_202_ACCEPTED)
+
 
 class ListFlashCardView(APIView):
-  permission_classes=(IsAuthenticated,)
-  def get(self , request, user_id):
-    user_all_flash_card = get_list_or_404(FlashCard,user__id=user_id)
-    
-    serializer = ListFlashCard(instance=user_all_flash_card, many=True)
+    permission_classes = (IsAuthenticated,)
 
-    return Response(serializer.data)
+    def get(self, request, user_id):
+        user_all_flash_card = get_list_or_404(FlashCard, user__id=user_id)
+
+        serializer = ListFlashCard(instance=user_all_flash_card, many=True)
+
+        return Response(serializer.data)
